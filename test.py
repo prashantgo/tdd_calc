1f08f9a2044f388caece019b0907013aa2d644fd
Prashant Goyal <prashant@xtenav.com>
2025-08-06 19:28:08 +0530

    Initial Setup with First Test Defined

---
+++ test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from calculator import add
+
+
+class TestAdd(unittest.TestCase):
+    def test_add(self):
+        """ Test that it can sum a string of integers """
+        strInput = ""
+        result = add(strInput)
+        self.assertEqual(result, 0)
+        
+        strInput = "1"
+        result = add(strInput)
+        self.assertEqual(result, 1)
+        
+        strInput = "1,5"
+        result = add(strInput)
+        self.assertEqual(result, 5)
+
+if __name__ == "__main_":
+    unittest.main()
