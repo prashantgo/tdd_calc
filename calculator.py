e57fe52f883e218c5d0a711873d95aa36bdac4ed
Prashant Goyal <prashant@xtenav.com>
2025-08-06 19:37:05 +0530

    feat: Added the functionality addNewLine to handle new line characters, added a new test case

--- calculator.py
+++ calculator.py
@@ -1,4 +1,13 @@
 def add(inputStr: str):
+    """Add the numbers separated by a comma in a string"""
     if not isinstance(inputStr, str): return 0
     if inputStr == "": return 0
+    return sum(map(lambda x: int(x), inputStr.split(",")))
+
+def addNewLine(inputStr: str):
+    """Add the numbers separated by a comma (new line included) in a string"""
+    if not isinstance(inputStr, str): return 0
+    if inputStr == "": return 0
+    
+    inputStr = inputStr.replace("\n", ",")
     return sum(map(lambda x: int(x), inputStr.split(",")))
--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import unittest
 
-from calculator import add
+from calculator import add, addNewLine
 
 
 class TestAdd(unittest.TestCase):
+
     def test_add(self):
         """ Test that it can sum a string of integers """
+
+        strInput = 47
+        result = add(strInput)
+        self.assertEqual(result, 0)
+
         strInput = ""
         result = add(strInput)
         self.assertEqual(result, 0)
@@ -17,6 +23,32 @@ class TestAdd(unittest.TestCase):
         strInput = "1,5"
         result = add(strInput)
         self.assertEqual(result, 6)
+    
+    def test_addNewLine(self):
+        """ Test that it can sum a string of integers with a new line separator """
+        strInput = 47
+        result = add(strInput)
+        self.assertEqual(result, 0)
+
+        strInput = ""
+        result = addNewLine(strInput)
+        self.assertEqual(result, 0)
+        
+        strInput = "1"
+        result = addNewLine(strInput)
+        self.assertEqual(result, 1)
+        
+        strInput = "1,5"
+        result = addNewLine(strInput)
+        self.assertEqual(result, 6)
+        
+        strInput = "1\n2,3"
+        result = addNewLine(strInput)
+        self.assertEqual(result, 6)
+        
+        strInput = "1\n2\n4"
+        result = addNewLine(strInput)
+        self.assertEqual(result, 7)
 
 if __name__ == "__main_":
     unittest.main()
