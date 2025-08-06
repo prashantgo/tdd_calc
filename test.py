6638857b195f33258ed3d974e04b2ed2ff23ce55
Prashant Goyal <prashant@xtenav.com>
2025-08-06 20:39:32 +0530

    chore: Formatting the test file using black

--- test.py
+++ test.py
@@ -5,83 +5,84 @@ from calculator import add, addNewLine, addAllowDelimiters, addDisallowNegative
 
 class TestAdd(unittest.TestCase):
 
-	def test_add(self):
-		""" Test that it can sum a string of integers """
+    def test_add(self):
+        """Test that it can sum a string of integers"""
 
-		testCases = {
-			47: 0,
-			"": 0,
-			"1": 1,
-			"1,5": 6,
-		}
+        testCases = {
+            47: 0,
+            "": 0,
+            "1": 1,
+            "1,5": 6,
+        }
 
-		for strInput, expectedResult in testCases.items():
-			result = add(strInput)
-			self.assertEqual(result, expectedResult)
-	
-	def test_addNewLine(self):
-		""" Test that it can sum a string of integers with a new line separator """
+        for strInput, expectedResult in testCases.items():
+            result = add(strInput)
+            self.assertEqual(result, expectedResult)
 
-		testCases = {
-			47: 0,
-			"": 0,
-			"1": 1,
-			"1,5": 6,
-			"1\n2,3": 6,
-			"1\n2\n4": 7,
-		}
+    def test_addNewLine(self):
+        """Test that it can sum a string of integers with a new line separator"""
 
-		for strInput, expectedResult in testCases.items():
-			result = addNewLine(strInput)
-			self.assertEqual(result, expectedResult)
+        testCases = {
+            47: 0,
+            "": 0,
+            "1": 1,
+            "1,5": 6,
+            "1\n2,3": 6,
+            "1\n2\n4": 7,
+        }
 
-	def test_addAllowDelimiters(self):
-		""" Test that it can sum a string of integers with a specified Delimiter """
+        for strInput, expectedResult in testCases.items():
+            result = addNewLine(strInput)
+            self.assertEqual(result, expectedResult)
 
-		testCases = {
-			47: 0,
-			"": 0,
-			"1": 0,
-			"//;": 0,
-			"//;\n": 0,
-			"//;\n1;2": 3,
-			"//-\n1-20": 21,
-			"//a\n4a3": 7,
-			"//a\n4aa3": 7,
-		}
+    def test_addAllowDelimiters(self):
+        """Test that it can sum a string of integers with a specified Delimiter"""
 
-		for strInput, expectedResult in testCases.items():
-			result = addAllowDelimiters(strInput)
-			self.assertEqual(result, expectedResult)
+        testCases = {
+            47: 0,
+            "": 0,
+            "1": 0,
+            "//;": 0,
+            "//;\n": 0,
+            "//;\n1;2": 3,
+            "//-\n1-20": 21,
+            "//a\n4a3": 7,
+            "//a\n4aa3": 7,
+        }
 
-	def test_addDisallowNegative(self):
-		""" Test that it can sum a string of integers with a specified Delimiter and negative numbers not allowed. """
+        for strInput, expectedResult in testCases.items():
+            result = addAllowDelimiters(strInput)
+            self.assertEqual(result, expectedResult)
 
-		positiveTestCases = {
-			47: 0,
-			"": 0,
-			"1": 0,
-			"//;": 0,
-			"//;\n": 0,
-			"//;\n1;2": 3,
-			"//-\n1-20": 21,
-			"//a\n4a3": 7,
-			"//a\n4aa3": 7,
-		}
+    def test_addDisallowNegative(self):
+        """Test that it can sum a string of integers with a specified Delimiter and negative numbers not allowed."""
 
-		for strInput, expectedResult in positiveTestCases.items():
-			result = addAllowDelimiters(strInput)
-			self.assertEqual(result, expectedResult)
+        positiveTestCases = {
+            47: 0,
+            "": 0,
+            "1": 0,
+            "//;": 0,
+            "//;\n": 0,
+            "//;\n1;2": 3,
+            "//-\n1-20": 21,
+            "//a\n4a3": 7,
+            "//a\n4aa3": 7,
+        }
 
-		negativeTestCases = [
-			"//;\n1;-2",
-			"//a\n-4a3",
-			"//a\n-4a-3",
-		]
+        for strInput, expectedResult in positiveTestCases.items():
+            result = addAllowDelimiters(strInput)
+            self.assertEqual(result, expectedResult)
 
-		for strInput in negativeTestCases:
-			with self.assertRaises(Exception):
-				_ = addDisallowNegative(strInput)
+        negativeTestCases = [
+            "//;\n1;-2",
+            "//a\n-4a3",
+            "//a\n-4a-3",
+        ]
+
+        for strInput in negativeTestCases:
+            with self.assertRaises(Exception):
+                _ = addDisallowNegative(strInput)
+
 
 if __name__ == "__main_":
-	unittest.main()
+    unittest.main()
