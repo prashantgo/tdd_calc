d48aeba1d0a0d87c3624a8372546182ceea36e7d
Prashant Goyal <prashant@xtenav.com>
2025-08-06 20:36:18 +0530

    feat: Add an add function to now allow Negative Numbers

--- calculator.py
+++ calculator.py
@@ -8,7 +8,7 @@ def addNewLine(inputStr: str):
     """Add the numbers separated by a comma (new line included) in a string"""
     if not isinstance(inputStr, str): return 0
     if inputStr == "": return 0
-    
+
     inputNumbers = inputStr.replace("\n", ",").split(",")
     return sum(map(lambda x: int(x), inputNumbers))
 
@@ -20,3 +20,19 @@ def addAllowDelimiters(inputStr: str):
     delimiter = inputStr[2]
     inputNumbers = inputStr[4:].split(delimiter)
     return sum([int(number) for number in inputNumbers if number.isdigit()])
+
+def addDisallowNegative(inputStr: str):
+    """Add the numbers separated by a delimiter in a string"""
+    if not isinstance(inputStr, str): return 0
+    if inputStr == "" or len(inputStr) <= 4: return 0
+
+    delimiter = inputStr[2]
+    inputNumbers = inputStr[4:].split(delimiter)
+    positiveNumbers = [int(number) for number in inputNumbers if number.lstrip("-").isdigit()]
+    negativeNumbers = [number for number in inputNumbers if number.lstrip("-").isdigit() and int(number) < 0]
+
+    if negativeNumbers:
+        negativeNumbers = ",".join(negativeNumbers)
+        raise Exception(f"negative numbers not allowed {negativeNumbers}")
+
+    return sum(positiveNumbers)
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from calculator import add, addNewLine, addAllowDelimiters
+from calculator import add, addNewLine, addAllowDelimiters, addDisallowNegative
 
 
 class TestAdd(unittest.TestCase):
@@ -53,6 +53,35 @@ class TestAdd(unittest.TestCase):
 		for strInput, expectedResult in testCases.items():
 			result = addAllowDelimiters(strInput)
 			self.assertEqual(result, expectedResult)
+
+	def test_addDisallowNegative(self):
+		""" Test that it can sum a string of integers with a specified Delimiter and negative numbers not allowed. """
+
+		positiveTestCases = {
+			47: 0,
+			"": 0,
+			"1": 0,
+			"//;": 0,
+			"//;\n": 0,
+			"//;\n1;2": 3,
+			"//-\n1-20": 21,
+			"//a\n4a3": 7,
+			"//a\n4aa3": 7,
+		}
+
+		for strInput, expectedResult in positiveTestCases.items():
+			result = addAllowDelimiters(strInput)
+			self.assertEqual(result, expectedResult)
+
+		negativeTestCases = [
+			"//;\n1;-2",
+			"//a\n-4a3",
+			"//a\n-4a-3",
+		]
+
+		for strInput in negativeTestCases:
+			with self.assertRaises(Exception):
+				_ = addDisallowNegative(strInput)
 
 if __name__ == "__main_":
 	unittest.main()
